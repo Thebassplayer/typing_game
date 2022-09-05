2343371f4fb6e81f9c6cc89553ad1ca3f5207b7b
Roy Lopez <windup.dues-0f@icloud.com>
2022-09-05 20:44:44 -0300

    HTML and CSS done

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const word = document.getElementById("word"),
+  text = document.getElementById("text"),
+  scoreEl = document.getElementById("score"),
+  timeEl = document.getElementById("time"),
+  endgameEl = document.getElementById("end.game"),
+  settingsBtn = document.getElementById("settings.btn"),
+  settings = document.getElementById("settings"),
+  settingsForm = document.getElementById("settings-form"),
+  difficultySelect = document.getElementById("difficulty"),
+  apiURL = "https://random-words-api.vercel.app/word";
 
+let randomWord;
+
+//! Fetch word API
+async function getWord() {
+  const res = await fetch(apiURL),
+    data = await res.json();
+  randomWord = data[0].word;
+}
+
+//! Init score
+let score = 0;
+
+//! Init time
+let time = 10;
+
+//! Add word to DOM
+function addWordToDOM() {
+  randomWord = getWord();
+}
+addWordToDOM();
